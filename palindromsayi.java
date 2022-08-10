d8a7dccf1da2a60c3cae90c7be3b358a762638d2
Ömer Alper Güneş <73720725+omeralpergunes@users.noreply.github.com>
2022-08-10 14:01:05 +0300

    Add files via upload

---
+++ palindromsayi.java
@@ -0,0 +1,35 @@
+package deneme1;
+
+import java.util.Scanner;
+
+public class palindromsayi {
+
+    static boolean isPalindrom(int number){
+        int temp=number,reverseNumber=0,lastNumber;
+
+        while(temp!=0){
+            lastNumber=temp%10;
+            reverseNumber=(reverseNumber*10)+lastNumber;
+            temp/=10;
+        }
+
+        if (number==reverseNumber)
+            return true;
+        else
+            return false;
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Palindrom olup olmadığını kontrol emek istediğiniz sayıyı giriniz:");
+        int number= input.nextInt();
+        System.out.println(isPalindrom(number));
+
+    }
+
+
+
+}
+
